3a88fd9cecbccd8a1a2bc234e1e0f026346391b5
GangeyaKumar <gk@gangeyakumars-macintosh.local>
2009-12-23 10:02:51 +0600

    remove unwanted codes

--- Myapp/Classes/Testapp.h
+++ Myapp/Classes/Testapp.h
@@ -9,25 +9,17 @@
 #import <UIKit/UIKit.h>
 #import<CoreGraphics/CoreGraphics.h>
 
-@interface Testapp : UIViewController <UIScrollViewDelegate>{
-
-	IBOutlet UITextField *textfield1;
-	IBOutlet UITextField *textfield2;
-	IBOutlet UITextField *textfield3;
-	IBOutlet UIButton *button1;
+@interface Testapp : UIViewController {
 	IBOutlet UIImageView *imageview1;
 	NSMutableArray *mycolor;
-	IBOutlet UIScrollView *aScrollView;
+	
 
 }
 
-@property (nonatomic, retain) IBOutlet UITextField *textfield1;
-@property (nonatomic, retain) UITextField *textfield2;
-@property (nonatomic, retain) UITextField *textfield3;
-@property (nonatomic, retain) UIButton *button1;
+
 @property (nonatomic, retain) IBOutlet UIImageView *imageview1;
 @property (nonatomic, retain) NSMutableArray *mycolor;
-@property (nonatomic, retain)  IBOutlet UIScrollView *aScrollView;
 
--(IBAction) click;
+
+
 @end
--- Myapp/Classes/Testapp.m
+++ Myapp/Classes/Testapp.m
@@ -8,13 +8,11 @@
 
 #import "Testapp.h"
 #import<CoreGraphics/CoreGraphics.h>
-#define IMAGE_WIDTH   320
-#define IMAGE_HEIGHT  480
+
 
 @implementation Testapp
-@synthesize textfield1,textfield2,textfield3,button1,imageview1,mycolor,aScrollView;
-int count=0;
-int count1;
+@synthesize imageview1,mycolor;
+
 /*
 // Override initWithNibName:bundle: to load the view using a nib file then perform additional customization that is not appropriate for viewDidLoad.
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
@@ -38,22 +36,7 @@ int count1;
 }
 */
 
--(IBAction) click
 
-{
-	//(NSInteger*) textfield3.text = (NSInteger*) (textfield1.text) + (NSInteger*) (textfield2.text);
-	NSLog(@"I am button");
-	int first = (int) [textfield1 text];
-	int second = (int) [textfield2 text];
-	int res; 
-	res = first+ second;
-	
-	
-	//int first = [textfield1 text];
-	//NSLog(@"result=%i",first);
-	//int valorConvertido = [strValor intValue];
- 	
-}
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
@@ -72,7 +55,7 @@ int count1;
 
 
 //To change the color
-/*- (void) touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event  {
+- (void) touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event  {
 	
 	//Declaring colors
 	UIColor *red =[UIColor redColor]; 
@@ -105,87 +88,16 @@ int count1;
 		//set the color
 		imageview1.backgroundColor=somecolor;
 	}	
-}*/
-
-//changing the images
-
-
-/*- (void) touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event  {
-	
-	//Declaring colors
-	
-	UIImage *img1 =[UIImage imageNamed:@"images-1.jpeg"];
-	UIImage *img2 =[UIImage imageNamed:@"images-2.jpeg"];
-	UIImage *img3 =[UIImage imageNamed:@"images-3.jpeg"];
-	UIImage *img4 =[UIImage imageNamed:@"images-4.jpeg"];
-	UIImage *img5 =[UIImage imageNamed:@"images-5.jpeg"];
-		
-    NSMutableArray *imagearray =[[NSMutableArray alloc] init];
-	//Storing colors in an array
-    imagearray = [NSMutableArray arrayWithObjects:img1,img2,img3,img4,img5,nil];
-	
-	UIImage *someimage;
-	//Increment the touches
-	NSUInteger mytouch  = [[touches anyObject] tapCount];
-	if(mytouch >=5)
-	{
-		mytouch=1;
-		//Retrieving the color from the array
-		someimage=(UIImage*) [imagearray objectAtIndex:mytouch-1];
-		//set the color
-		imageview1.image=someimage;
-		
-	}
-	else
-	{
-		someimage=(UIImage*) [imagearray objectAtIndex:mytouch-1];
-		//set the color
-		imageview1.image=someimage;
-		
-	}	
-	
-}*/
-
-
-
-
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	
-   /* NSArray *photos = nil;      // TODO – fill with your photos
-	
-    // note that the view contains a UIScrollView in aScrollView
-	
-	int i=0;
-	for ( NSString *image in photos )
-	{*/
-	int i=0;
-	UIImage *img1 =[UIImage imageNamed:@"images-1.jpeg"];
-	UIImage *img2 =[UIImage imageNamed:@"images-2.jpeg"];
-	UIImage *img3 =[UIImage imageNamed:@"images-3.jpeg"];
-	UIImage *img4 =[UIImage imageNamed:@"images-4.jpeg"];
-	UIImage *img5 =[UIImage imageNamed:@"images-5.jpeg"];
-	
-    NSMutableArray *imagearray =[[NSMutableArray alloc] init];
-	//Storing colors in an array
-    imagearray = [NSMutableArray arrayWithObjects:img1,img2,img3,img4,img5,nil];
-	for ( NSString *image in imagearray )
-	{
-		UIImage *image = [imagearray objectAtIndex:i];
-		//UIImage *image = [UIImage imageNamed:@"images-1.jpeg"];
-		imageview1 = [[UIImageView alloc] initWithImage:image];
-		//imageview1.contentMode = UIViewContentModeScaleAspectFit;
-		//imageview1.clipsToBounds = YES;
-		
-		imageview1.frame = CGRectMake( IMAGE_WIDTH * i++, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
-		
-		[aScrollView addSubview:imageview1];
-		[imageview1 release];
-	}
-	aScrollView.contentSize = CGSizeMake(IMAGE_WIDTH*i, IMAGE_HEIGHT);
-	aScrollView.delegate = self;
 }
 
+
+
+
+
+/*- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+*/
 @end
